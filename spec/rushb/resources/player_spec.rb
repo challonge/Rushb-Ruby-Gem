1209a8367094b2569c689d73439463c558e6aade
Adrian Peterson Co <adco@splitmedialabs.com>
2019-09-30 21:04:37 +0800

    Finishes the migration for Player API

--- spec/rushb/resources/player_spec.rb
+++ spec/rushb/resources/player_spec.rb
@@ -4,7 +4,7 @@ require 'rushb'
 require_relative '../../support/vcr.rb'
 
 RSpec.describe Rushb::Resources::Player, type: :client, vcr: true do
-  let!(:client) { Rushb::Client.new('4cdb7f00862e0133005b033abce37ff2') }
+  let!(:client) { Rushb::V2::Client.new('4cdb7f00862e0133005b033abce37ff2') }
   let!(:game_id) { 169961 }
 
   describe "#players" do
@@ -12,7 +12,6 @@ RSpec.describe Rushb::Resources::Player, type: :client, vcr: true do
       response = client.players(game_id)
 
       expect(response.count).to eq 2
-      expect(response.class).to eq WillPaginate::Collection
       expect(response.first["id"]).to eq 568368
       expect(response.first["pro"]).to be_falsey
       expect(response.first["rating"]).to eq 1012
@@ -22,7 +21,15 @@ RSpec.describe Rushb::Resources::Player, type: :client, vcr: true do
       response = client.players(game_id, { page: 1, per_page: 1 })
 
       expect(response.count).to eq 1
-      expect(response.class).to eq WillPaginate::Collection
+      expect(response.first["id"]).to eq 568368
+      expect(response.first["pro"]).to be_falsey
+      expect(response.first["rating"]).to eq 1012
+    end
+
+    it "returns the players for that game with options (player_ids)" do
+      response = client.players(game_id, { page: 1, per_page: 1, ids: 568368 })
+
+      expect(response.count).to eq 1
       expect(response.first["id"]).to eq 568368
       expect(response.first["pro"]).to be_falsey
       expect(response.first["rating"]).to eq 1012
@@ -33,7 +40,7 @@ RSpec.describe Rushb::Resources::Player, type: :client, vcr: true do
     it "returns the player details" do
       response = client.show_player(game_id, 568368)
 
-      expect(response.class).to eq Rushb::Smash::Player
+      expect(response.class).to eq Hash
       expect(response["id"]).to eq 568368
       expect(response["pro"]).to be_falsey
       expect(response["rating"]).to eq 1012
@@ -44,7 +51,7 @@ RSpec.describe Rushb::Resources::Player, type: :client, vcr: true do
     it "creates a player" do
       response = client.create_player(game_id)
 
-      expect(response.class).to eq Rushb::Smash::Player
+      expect(response.class).to eq Hash
       expect(response["id"]).to eq 596484
       expect(response["pro"]).to be_falsey
       expect(response["rating"]).to eq 1000
@@ -53,7 +60,7 @@ RSpec.describe Rushb::Resources::Player, type: :client, vcr: true do
     it "creates a player with params" do
       response = client.create_player(game_id, { pro: true })
 
-      expect(response.class).to eq Rushb::Smash::Player
+      expect(response.class).to eq Hash
       expect(response["id"]).to eq 596485
       expect(response["pro"]).to be_falsey
       expect(response["rating"]).to eq 1000
@@ -64,7 +71,7 @@ RSpec.describe Rushb::Resources::Player, type: :client, vcr: true do
     it "deletes the player" do
       response = client.destroy_player(game_id, 596485)
 
-      expect(response.class).to eq Rushb::Smash::Player
+      expect(response.class).to eq Hash
       expect(response["id"]).to eq 596485
     end
   end
@@ -74,7 +81,7 @@ RSpec.describe Rushb::Resources::Player, type: :client, vcr: true do
       response = client.player_rankings(game_id, 568368)
 
       expect(response.count).to eq 1
-      expect(response.class).to eq WillPaginate::Collection
+      expect(response.class).to eq Array
       expect(response.first["rating"]).to eq 1012
     end
 
@@ -82,7 +89,7 @@ RSpec.describe Rushb::Resources::Player, type: :client, vcr: true do
       response = client.player_rankings(game_id, 568368, { page: 1, per_page: 1 })
 
       expect(response.count).to eq 1
-      expect(response.class).to eq WillPaginate::Collection
+      expect(response.class).to eq Array
       expect(response.first["rating"]).to eq 1012
     end
   end
